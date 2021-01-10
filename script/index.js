86e04d53427a086d5733dc192bc56e175867c4e2
lb madesia <lmadesia@gmail.com>
2021-01-11 01:18:07 +0530

    complet validation form you can use this code any project

--- script/index.js
+++ script/index.js
@@ -1,118 +1,55 @@
-// input code here
-const input = document.getElementsByClassName("otp");
-const btn = document.getElementById("btn");
+const allinput = document.getElementsByClassName("otp");
 
-function infunn(){
-   for(var i=0;i<input.length;++i){
-       input[i].onkeyup = function(e){
-           if(e.ctrlKey && e.keyCode == 86){
-               var data = this.value;
-                for(var j=0;j<6;++j){
-                  input[j].value = data[j];
-                    alert(data);
-               }  
-               }
-       }
-       input[i].onpaste = function(){
-           var tag = this;
-           setTimeout(function(){fpaste(tag.value)},100);
-       }
-       input[i].onkeydown = function(e){
-    //left arrow key code 
-    if (e.keyCode == 37) { 
-       var intag = Number(sessionStorage.getItem("ifocus"));
-         if(intag <= 0){
-              input[5].focus();
-             sessionStorage.setItem("ifocus",6);
+for(var i=0;i<allinput.length;++i){
+    allinput[i].onkeyup = function(e){
+        if(e.keyCode == 8){
+            back(this);
+        }
+        else if (e.keyCode == 37) { 
+            back(this);
+        }
+        else if (e.keyCode == 39) {
+            jump(this);
+        }
+        else if(e.ctrlKey && e.keyCode == 86){
+            pasteData(this.value);
          }
          else{
-             --intag;
-         input[intag].focus();
-              sessionStorage.setItem("ifocus",intag);
-         }
+            jump(this);
+        }
     }
-    //left arrow key code
-    else if (e.keyCode == 39) { 
-       var intag = Number(sessionStorage.getItem("ifocus"));
-         if(intag >= 6){
-              input[0].focus();
-             sessionStorage.setItem("ifocus",1);
-         }
-         else{
-         input[intag].focus();
-             ++intag;
-              sessionStorage.setItem("ifocus",intag);
-         }
-    }
-    else if(e.keyCode == 13){
-         // enter key code
-         var intag = Number(sessionStorage.getItem("ifocus"));
-         if(intag >= 6){
-              btn.click();
-         }
-         else{
-         input[intag].focus();
-             ++intag;
-              sessionStorage.setItem("ifocus",intag);
-         }
-     }
-    else if(e.keyCode == 8){
-       var intag = Number(sessionStorage.getItem("ifocus"));
-        if(intag == 6){
-            -- intag;
-           }
-         if(input[intag].value == ""){
-             --intag;
-         input[intag].focus();
-              sessionStorage.setItem("ifocus",intag);
-         }
-    }
-    else if(e.ctrlKey && e.keyCode == 86){
-       // alert(this.value);
-    }
-    else{
-        autococus(this);
-    }
-           
 }
-     input[i].onclick = function(){
-         var ind = this.getAttribute("dindex");
-          sessionStorage.setItem("ifocus",ind);
-      } 
-   
-};
-    
-} 
-
-
-// start auto focus input code
-function autococus(tag){
-    var ind = Number(tag.getAttribute("dindex"));
-    sessionStorage.setItem("ifocus",ind);
-           if(tag.value.length >= 1){
-            if(ind != 6){
-               input[ind].focus();
+// code to move focus to another input
+    function jump(tag){
+             var index = Number(tag.getAttribute("dindex"));
+            if(index == 6){
+                confirm();
             }
-       }
+            else{
+                allinput[index].focus();
+            }
+    }
+ 
+ //===========================================================================================================
+
+ // code to delete value and jump focus in previous input when press backspace
+function back(tag){
+        var index = Number(tag.getAttribute("dindex"));
+         if(index != 1){
+            index -=2;
+            allinput[index].focus();
+         }
 }
-
-
-
-
-   infunn();
-// confirm code
-function confirm(){
-    sessionStorage.removeItem("ifocus");
-    var alert = document.getElementById("alert");
-      btn.focus();
-    alert.style.display = "block";
-    setTimeout(function(){alert.style.display = "none";},4000);
+// ===================================================================
+// start paste data from clipboard
+function pasteData(tag){
+     for(var i=0;i<tag.length && i<6;++i){
+        allinput[i].value = tag[i];
+      }
 }
+//=================================================================
 
-//paste otp
-function fpaste(tag){
-      for(var j=0;j<6;++j){
-       input[j].value = tag[j];
-     }
-}
-    // end paste code
+    // start submit here
+    function confirm(){
+        alert("confirm");
+    }
